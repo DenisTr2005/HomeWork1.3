5acf8d50cd9abb2834e10ef9770b86521c610e66
DenisTr2005 <den_2005@list.ru>
2022-04-20 18:00:21 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите 0 или 1: ");
+        int clientOS = in.nextInt();
+        in.close();
+        boolean isOSAndroid = clientOS == 1;
+        if (isOSAndroid) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
     }
 }
